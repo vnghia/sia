1e8ed6eeceeeaa4888a14210f95c4f298814fc75
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2021-08-19 23:33:30 +0200

    room: use end for center negative y

--- World/Welcome/Welcome.gd
+++ World/Welcome/Welcome.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 func _enter_tree():
-	var size = $WallFloor.get_used_rect().size
-	var real_pos = $WallFloor.map_to_world(Vector2(size.x, size.y - 1))
+	var end = $WallFloor.get_used_rect().end
+	var real_pos = $WallFloor.map_to_world(Vector2(end.x, end.y - 1))
 	Character_Globals.CharacterNode.position = Vector2(real_pos.x / 2, real_pos.y)
