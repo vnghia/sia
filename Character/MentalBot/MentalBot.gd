79855ec96bf2f417ca675461a3d151f7027e0b4a
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2021-08-20 15:36:21 +0200

    world: fix screen resolution

--- Character/MentalBot/MentalBot.gd
+++ Character/MentalBot/MentalBot.gd
@@ -4,4 +4,9 @@ extends KinematicBody2D
 func _ready():
 	$Idle.texture = load("res://Character/MentalBot/idle.png")
 	$Run.texture = load("res://Character/MentalBot/run.png")
-	self.position = Vector2(self.get_viewport().size.x / 2, self.get_viewport().size.y / 2)
+	var scenes = World_Globals.list_scenes
+	var tile_map = scenes["Welcome"]["instance"].get_node("WallFloor")
+	var used_rect = tile_map.get_used_rect()
+	var pos = tile_map.map_to_world(used_rect.position)
+	var end = tile_map.map_to_world(used_rect.end)
+	self.position = (pos + end) / 2
