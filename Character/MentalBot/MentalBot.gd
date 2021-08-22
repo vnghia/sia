dfbdf8d3d5a97241bfa428af33171fb1c07a0cba
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2021-08-22 14:12:46 +0200

    control: allow user interface on top of character

--- Character/MentalBot/MentalBot.gd
+++ Character/MentalBot/MentalBot.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+var current_state = "begin"
+
 
 func _init():
 	self.set_meta("name", "MentalBot")
@@ -17,9 +19,17 @@ func _ready():
 
 
 func interact():
-	World_Globals.dialog_system.print_dialog(
-		self.get_meta("name"),
-		"Hello {0} ! Welcome Aboard ! I'm MentalBot.".format(
-			[Player_Globals.player_node.get_meta("name")]
+	if current_state == "begin":
+		print(current_state)
+		World_Globals.dialog_system.print_dialog(
+			self.get_meta("name"),
+			"Hello {0} ! How are you feeling today ?".format(
+				[Player_Globals.player_node.get_meta("name")]
+			)
 		)
-	)
+		$UI.switch("love")
+		current_state = "end"
+	else:
+		World_Globals.dialog_system.print_dialog(
+			self.get_meta("name"), "Thank you for telling me your feeling !"
+		)
